dc96a904ca0a7c17032bf7b150a9d8180faa1dc0
david <davidal.berrocallengua@gmail.com>
2026-07-14 13:11:31 -0500

    asdfasdf

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const pgPool = new Pool({
 // ============================================================
 // 2. CONEXIÓN A MONGODB ATLAS
 // ============================================================
-const MONGO_URI = 'mongodb+srv://david_:gS:DbX29QzBWei9@rodillacluster.xtchrmj.mongodb.net/?appName=RodillaCluster';
+const MONGO_URI = 'mongodb+srv://david_:F84IyNpiujGUPA2e@rodillacluster.xtchrmj.mongodb.net/?appName=RodillaCluster';
 const mongoClient = new MongoClient(MONGO_URI);
 let mongoDB;
 
